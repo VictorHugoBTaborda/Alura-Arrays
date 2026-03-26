f1e70f1e92d2f8d9cde8ef33cd39bba91cecabd6
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2026-03-26 08:38:56 -0300

    media.js

--- media.js
+++ media.js
@@ -3,8 +3,12 @@
 // const nota3 = 8;
 // const nota4 = 7.5;
 
+
+//posições:
+//              0   1   2   3
 const notas = [10, 6.5, 8, 7.5]
 
-const media = (nota1 + nota2 + nota3 + nota4) / 4;
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.lenght;
 
 console.log(media);
+
