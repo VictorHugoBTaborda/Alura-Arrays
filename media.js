48442a5e33c98638197d21b9a28c73ce7291f0c0
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2026-03-26 08:49:09 -0300

    media.js

--- media.js
+++ media.js
@@ -8,7 +8,7 @@
 //              0   1   2   3
 const notas = [10, 6.5, 8, 7.5]
 
-const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.lenght;
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media);
 // alura video 4, oque sao arrays
