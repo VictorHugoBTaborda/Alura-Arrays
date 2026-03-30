0c34731943ac84f7d4e24f07a4fd97e47c4e7677
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2026-03-30 10:11:32 -0300

    lista-duas-dimensoes.js

--- juntando-salas.js
+++ juntando-salas.js
@@ -1,6 +1,6 @@
 const salaJS = ["Evaldo", "Camis", "Mari"];
 const salaPython = ["Ju", "Leo", "Raquel"];
 
-const salasUnificadas = salaJS.concat(salaPython);
+const salasUnificadas = salaPython.concat(salaJS);
 
 console.log(salasUnificadas);
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,2 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
