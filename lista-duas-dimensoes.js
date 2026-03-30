3dc6e258760dd35113de9f3b15b2e2a6c57d2241
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2026-03-30 10:18:08 -0300

    lista-duas-dimensoes.js

--- lista-duas-dimensoes.js
+++ lista-duas-dimensoes.js
@@ -1,2 +1,7 @@
 const alunos = ["João", "Juliana", "Ana", "Caio"];
 const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log('A aluna da
+    //lista com duas dimnensoes video
